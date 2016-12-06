69042289bb0dff67090ebcecc51dc8165a906a42
Júlio Hoffimann <julio.hoffimann@gmail.com>
2016-12-06 13:20:54 -0800

    Use test sets in test suite

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,30 +8,34 @@ dim = 3; nobs = 10
 X = rand(dim, nobs); z = rand(nobs)
 x₀ = rand(dim)
 
-# Kriging is an interpolator
-for j=1:nobs
-  SKestimate, SKvar = kriging(X[:,j], X, z, μ=mean(z))
-  OKestimate, OKvar = kriging(X[:,j], X, z)
-  UKestimate, UKvar = unikrig(X[:,j], X, z, degree=1)
+@testset "Basic checks" begin
+  # Kriging is an interpolator
+  for j=1:nobs
+    SKestimate, SKvar = kriging(X[:,j], X, z, μ=mean(z))
+    OKestimate, OKvar = kriging(X[:,j], X, z)
+    UKestimate, UKvar = unikrig(X[:,j], X, z, degree=1)
 
-  @test isapprox(SKestimate, z[j])
-  @test isapprox(OKestimate, z[j])
-  @test isapprox(UKestimate, z[j])
+    @test isapprox(SKestimate, z[j])
+    @test isapprox(OKestimate, z[j])
+    @test isapprox(UKestimate, z[j])
 
-  # variance checks
-  @test SKvar + tol ≥ 0
-  @test OKvar + tol ≥ 0
-  @test SKvar ≤ OKvar + tol
+    # variance checks
+    @test SKvar + tol ≥ 0
+    @test OKvar + tol ≥ 0
+    @test SKvar ≤ OKvar + tol
+  end
+
+  # Ordinary Kriging ≡ Universal Kriging with 0th degree drift
+  OKestimate, OKvar = kriging(x₀, X, z)
+  UKestimate, UKvar = unikrig(x₀, X, z, degree=0)
+  @test isapprox(OKestimate, UKestimate)
+  @test isapprox(OKvar, UKvar)
 end
 
-# Ordinary Kriging ≡ Universal Kriging with 0th degree drift
-OKestimate, OKvar = kriging(x₀, X, z)
-UKestimate, UKvar = unikrig(x₀, X, z, degree=0)
-@test isapprox(OKestimate, UKestimate)
-@test isapprox(OKvar, UKvar)
-
-# covariance is decreasing
-h = linspace(0,100)
-@test all(CovarianceModel.gaussian(h) .≥ CovarianceModel.gaussian(h+1))
-@test all(CovarianceModel.spherical(h) .≥ CovarianceModel.spherical(h+1))
-@test all(CovarianceModel.exponential(h) .≥ CovarianceModel.exponential(h+1))
+@testset "Covariance models" begin
+  # covariance is decreasing
+  h = linspace(0,100)
+  @test all(CovarianceModel.gaussian(h) .≥ CovarianceModel.gaussian(h+1))
+  @test all(CovarianceModel.spherical(h) .≥ CovarianceModel.spherical(h+1))
+  @test all(CovarianceModel.exponential(h) .≥ CovarianceModel.exponential(h+1))
+end
