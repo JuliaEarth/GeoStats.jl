ea3ef8a0bcd3fe197e0c056e1ca84a64a45e2261
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-05-07 18:13:44 -0700

    Use Cholesky factorization in Simple Kriging

--- src/simple_kriging.jl
+++ src/simple_kriging.jl
@@ -30,7 +30,7 @@ type SimpleKriging{T<:Real,V} <: AbstractEstimator
   μ::V
 
   # state fields
-  LU::Base.LinAlg.Factorization{T}
+  LLᵀ::Base.LinAlg.Factorization{T}
 
   function SimpleKriging(X, z, cov, μ)
     @assert size(X, 2) == length(z) "incorrect data configuration"
@@ -45,17 +45,13 @@ SimpleKriging(X, z, cov, μ) = SimpleKriging{eltype(X),eltype(z)}(X, z, cov, μ)
 function fit!{T<:Real,V}(estimator::SimpleKriging{T,V}, X::AbstractMatrix{T})
   estimator.X = X
   C = pairwise(estimator.cov, X)
-  if isposdef(C)
-    estimator.LU = cholfact(C)
-  else
-    estimator.LU = lufact(C)
-  end
+  estimator.LLᵀ = cholfact(C)
 end
 
 function estimate{T<:Real,V}(estimator::SimpleKriging{T,V}, xₒ::AbstractVector{T})
   X = estimator.X; z = estimator.z
   cov = estimator.cov; μ = estimator.μ
-  LU = estimator.LU
+  LLᵀ = estimator.LLᵀ
   nobs = length(z)
 
   # evaluate covariance at location
@@ -63,7 +59,7 @@ function estimate{T<:Real,V}(estimator::SimpleKriging{T,V}, xₒ::AbstractVector
 
   # solve linear system
   y = z - μ
-  λ = LU \ c
+  λ = LLᵀ \ c
 
   # return estimate and variance
   μ + y⋅λ, cov(0) - c⋅λ
