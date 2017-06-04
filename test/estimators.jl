f22a7cbfac64ccd3d893a5da250ed80138abb167
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-06-03 17:48:14 -0700

    Refactor tests for Kriging estimators

--- test/estimators.jl
+++ test/estimators.jl
@@ -1,5 +1,5 @@
 @testset "Kriging" begin
-  γ = GaussianVariogram()
+  γ = GaussianVariogram(1., 1., 0.)
   simkrig = SimpleKriging(X, z, γ, mean(z))
   ordkrig = OrdinaryKriging(X, z, γ)
   unikrig = UniversalKriging(X, z, γ, 1)
@@ -11,9 +11,9 @@
     UKestimate, UKvar = estimate(unikrig, X[:,j])
 
     # estimate checks
-    @test isapprox(SKestimate, z[j])
-    @test isapprox(OKestimate, z[j])
-    @test isapprox(UKestimate, z[j])
+    @test SKestimate ≈ z[j]
+    @test OKestimate ≈ z[j]
+    @test UKestimate ≈ z[j]
 
     # variance checks
     @test SKvar + tol ≥ 0
@@ -34,29 +34,29 @@
   SKestimate_h, SKvar_h = estimate(simkrig_h, xₒ .+ h)
   OKestimate_h, OKvar_h = estimate(ordkrig_h, xₒ .+ h)
   UKestimate_h, UKvar_h = estimate(unikrig_h, xₒ .+ h)
-  @test isapprox(SKestimate, SKestimate_h)
-  @test isapprox(SKvar, SKvar_h)
-  @test isapprox(OKestimate, OKestimate_h)
-  @test isapprox(OKvar, OKvar_h)
-  @test isapprox(UKestimate, UKestimate_h)
-  @test isapprox(UKvar, UKvar_h)
+  @test SKestimate_h ≈ SKestimate
+  @test SKvar_h ≈ SKvar
+  @test OKestimate_h ≈ OKestimate
+  @test OKvar_h ≈ OKvar
+  @test UKestimate_h ≈ UKestimate
+  @test UKvar_h ≈ UKvar
 
   # Kriging estimate is invariant under covariance scaling
   # Kriging variance is multiplied by the same factor
   α = rand()
-  γ_α = GaussianVariogram(α, 1.)
+  γ_α = GaussianVariogram(α, 1., 0.)
   simkrig_α = SimpleKriging(X, z, γ_α, mean(z))
   ordkrig_α = OrdinaryKriging(X, z, γ_α)
   unikrig_α = UniversalKriging(X, z, γ_α, 1)
   SKestimate_α, SKvar_α = estimate(simkrig_α, xₒ)
   OKestimate_α, OKvar_α = estimate(ordkrig_α, xₒ)
   UKestimate_α, UKvar_α = estimate(unikrig_α, xₒ)
-  @test isapprox(SKestimate_α, SKestimate)
-  @test isapprox(SKvar_α, α*SKvar)
-  @test isapprox(OKestimate_α, OKestimate)
-  @test isapprox(OKvar_α, α*OKvar)
-  @test isapprox(UKestimate_α, UKestimate)
-  @test isapprox(UKvar_α, α*UKvar)
+  @test SKestimate_α ≈ SKestimate
+  @test SKvar_α ≈ α*SKvar
+  @test OKestimate_α ≈ OKestimate
+  @test OKvar_α ≈ α*OKvar
+  @test UKestimate_α ≈ UKestimate
+  @test UKvar_α ≈ α*UKvar
 
   # Kriging variance is a function of data configuration, not data values
   δ = rand(nobs)
@@ -67,14 +67,14 @@
   SKestimate_δ, SKvar_δ = estimate(simkrig_δ, xₒ)
   OKestimate_δ, OKvar_δ = estimate(ordkrig_δ, xₒ)
   UKestimate_δ, UKvar_δ = estimate(unikrig_δ, xₒ)
-  @test isapprox(SKvar, SKvar_δ)
-  @test isapprox(OKvar, OKvar_δ)
-  @test isapprox(UKvar, UKvar_δ)
+  @test SKvar_δ ≈ SKvar
+  @test OKvar_δ ≈ OKvar
+  @test UKvar_δ ≈ UKvar
 
   # Ordinary Kriging ≡ Universal Kriging with 0th degree drift
   unikrig_0th = UniversalKriging(X, z, γ, 0)
   OKestimate, OKvar = estimate(ordkrig, xₒ)
   UKestimate, UKvar = estimate(unikrig_0th, xₒ)
-  @test isapprox(OKestimate, UKestimate)
-  @test isapprox(OKvar, UKvar)
+  @test OKestimate ≈ UKestimate
+  @test OKvar ≈ UKvar
 end
