e456d58adc2530203c85c0620446240a28c91b25
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-08-12 12:16:43 -0700

    Add test for GeoDataFrame's npoints

--- test/geodataframe.jl
+++ test/geodataframe.jl
@@ -2,5 +2,6 @@
   @test names(data(data3D)) == [:x,:y,:z,:value]
   @test coordnames(data3D) == [:x,:y,:z]
   @test names(coordinates(data3D)) == coordnames(data3D)
+  @test npoints(data3D) == 100
   @test_throws AssertionError readtable(fname, coordnames=[:a])
 end
