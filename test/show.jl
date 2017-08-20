b5ca89ca1d771f386d8c9eb4ca7ecf31a845ae7d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-08-19 23:44:00 -0700

    Delete test/show.jl

--- test/show.jl
+++
@@ -1,8 +0,0 @@
-@testset "Show methods" begin
-  @testset "SimulationProblem" begin
-    grid2D = RegularGrid{Float64}(100,200)
-    problem = SimulationProblem(data2D, grid2D, :value, 100)
-    @test sprint(show, problem) == "2D SimulationProblem (conditional)"
-    @test sprint(show, MIME"text/plain"(), problem) == "2D SimulationProblem (conditional)\n  data:      3×3 GeoDataFrame (x and y)\n  domain:    100×200 RegularGrid{Float64,2}\n  variables: value\n  N° reals:  100"
-  end
-end
