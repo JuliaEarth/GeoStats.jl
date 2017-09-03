88608542ae9835d9875d27937bf61e9e2159d3d0
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-09-02 18:46:15 -0700

    Add tests for BallNeighborhood

--- test/neighborhoods.jl
+++ test/neighborhoods.jl
@@ -3,9 +3,19 @@
     grid = RegularGrid{Float64}(100,100)
 
     neigh1 = GeoStats.CubeNeighborhood(grid, 1.)
-    @test neigh1(1) == [1]
+    @test neigh1(1) == [1,2,101,102]
 
     neigh2 = GeoStats.CubeNeighborhood(grid, 2.)
-    @test neigh2(1) == [1,2,101,102]
+    @test neigh2(1) == [1,2,3,101,102,103,201,202,203]
+  end
+
+  @testset "BallNeighborhood" begin
+    grid = RegularGrid{Float64}(100,100)
+
+    neigh1 = GeoStats.BallNeighborhood(grid, 1.)
+    @test neigh1(1) == [1,2,101]
+
+    neigh2 = GeoStats.BallNeighborhood(grid, .5)
+    @test neigh2(1) == [1]
   end
 end
