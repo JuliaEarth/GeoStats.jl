c8c7fe9250f34057287697529cac6fba1ffed2f1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-09-02 20:17:40 -0700

    Add more tests for solvers

--- test/solvers.jl
+++ test/solvers.jl
@@ -1,7 +1,7 @@
 @testset "Solvers" begin
   grid = RegularGrid{Float64}(100,100)
 
-  @testset "Kriging solver" begin
+  @testset "Kriging" begin
     problem = EstimationProblem(data2D, grid, :value)
     solver = Kriging(:value => @NT(variogram=GaussianVariogram(range=20.)))
 
@@ -10,6 +10,10 @@
     # basic checks
     result = digest(solution)
     @test Set(keys(result)) == Set([:value])
+    # TODO: check why the accuracy is low (≈ 1e-1)
+    @test isapprox(result[:value][:mean][25,25], 1., atol=1e-1)
+    @test isapprox(result[:value][:mean][50,75], 0., atol=1e-1)
+    @test isapprox(result[:value][:mean][75,50], 1., atol=1e-1)
 
     if ismaintainer || istravis
       @testset "Plot recipe" begin
@@ -23,11 +27,18 @@
     end
   end
 
-  @testset "SeqGaussSim solver" begin
+  @testset "SeqGaussSim" begin
     # conditional simulation with SeqGaussSim
     problem = SimulationProblem(data2D, grid, :value, 1)
-    #solution = solve(problem, SeqGaussSim())
-    # TODO: test solution correctness
+    solver = SeqGaussSim(:value => @NT(variogram=GaussianVariogram(range=20.)))
+    solution = solve(problem, solver)
+
+    # basic checks
+    result = digest(solution)
+    @test Set(keys(result)) == Set([:value])
+    @test result[:value][1][25,25] == 1.
+    @test result[:value][1][50,75] == 0.
+    @test result[:value][1][75,50] == 1.
 
     # unconditional simulation with SeqGaussSim
     problem = SimulationProblem(grid, :value => Float64, 1)
