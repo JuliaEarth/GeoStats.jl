82cc0a5853055fc8600237f139ab649a5756b8f1
Júlio Hoffimann <julio.hoffimann@gmail.com>
2017-12-31 12:45:07 -0800

    Use quartiles of distance as default lags in h-scatter plot

--- src/plotrecipes/hscatter.jl
+++ src/plotrecipes/hscatter.jl
@@ -14,18 +14,23 @@
 
 @userplot HScatter
 
-@recipe function f(hs::HScatter; lags=[1.], tol=1e-1, distance=Euclidean())
+@recipe function f(hs::HScatter; lags=nothing, tol=1e-1, distance=Euclidean())
   # get inputs
   spatialdata = hs.args[1]
   var₁ = hs.args[2]
   var₂ = length(hs.args) == 3 ? hs.args[3] : var₁
 
+  # lookup valid data
   X₁, z₁ = valid(spatialdata, var₁)
   X₂, z₂ = valid(spatialdata, var₂)
 
+  # compute pairwise distance
   m, n = length(z₁), length(z₂)
   ds = [evaluate(distance, view(X₁,:,i), view(X₂,:,j)) for j in 1:n for i in j:m]
   inds = [sub2ind((m, n), i, j) for j in 1:n for i in j:m]
+
+  # use quartiles by default
+  lags == nothing && (lags = quantile(ds, [.00,.25,.50,.75]))
 
   xlabel := var₁
   ylabel := var₂
@@ -33,10 +38,12 @@
   aspect_ratio := :equal
   layout --> (1, length(lags))
 
-  for (i,lag) in enumerate(lags)
+  for (i, lag) in enumerate(lags)
+    # find indices with given lag
     match = find(abs.(ds - lag) .< tol)
     ind₁, ind₂ = ind2sub((m, n), inds[match])
 
+    # scatter plot coordinates
     x, y = z₁[ind₁], z₂[ind₂]
 
     # plot identity line
