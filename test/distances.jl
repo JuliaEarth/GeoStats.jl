75a811b3c60d14fa1501d81d09d151792e5974f9
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-01-01 23:33:26 -0800

    Increase test coverage

--- test/distances.jl
+++ test/distances.jl
@@ -1,5 +1,10 @@
 @testset "Distances" begin
-  dist = Ellipsoidal([1.,.5,.5], [π/4,0.,0.])
-  @test evaluate(dist, [1.,1.,0.], [0.,0.,0.]) ≈ √2
-  @test evaluate(dist, [-1.,1.,0.], [0.,0.,0.]) ≈ √8
+  d₁ = Ellipsoidal([1.,1.], [0.])
+  d₂ = Ellipsoidal([1.,2.], [0.])
+  @test evaluate(d₁, [1.,0.], [0.,0.]) == evaluate(d₁, [0.,1.], [0.,0.])
+  @test evaluate(d₂, [1.,0.], [0.,0.]) != evaluate(d₂, [0.,1.], [0.,0.])
+
+  d₃ = Ellipsoidal([1.,.5,.5], [π/4,0.,0.])
+  @test evaluate(d₃, [1.,1.,0.], [0.,0.,0.]) ≈ √2
+  @test evaluate(d₃, [-1.,1.,0.], [0.,0.,0.]) ≈ √8
 end
