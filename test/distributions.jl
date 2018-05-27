6654b4f48ba4e2df9859f6b545cea3895cdd2e35
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-05-27 16:57:15 -0700

    Migrate tests to GeoStatsDevTools.jl

--- test/distributions.jl
+++
@@ -1,7 +0,0 @@
-@testset "Distributions" begin
-  # empirical distribution
-  values = randn(1000)
-  d = EmpiricalDistribution(values)
-  @test 0. ≤ cdf(d, rand()) ≤ 1.
-  @test minimum(values) ≤ quantile(d, .5) ≤ maximum(values)
-end
