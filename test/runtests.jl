769d5b7ca51102b49fff7c1d0ed157b94e20b78d
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-05-27 16:57:15 -0700

    Migrate tests to GeoStatsDevTools.jl
    Former-commit-id: 6654b4f48ba4e2df9859f6b545cea3895cdd2e35

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,7 @@
 using GeoStats
-using GeoStatsImages
 using Plots; gr(size=(600,400))
-using Base.Test
 using VisualRegressionTests
+using Base.Test
 
 # setup GR backend for Travis CI
 ENV["GKSwstype"] = "100"
@@ -16,35 +15,14 @@ istravis = "TRAVIS" ∈ keys(ENV)
 ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
 datadir = joinpath(@__DIR__,"data")
 
-# very simple data
-fname1D = joinpath(datadir,"data1D.tsv")
-data1D = readtable(fname1D, delim='\t', coordnames=[:x])
-fname2D = joinpath(datadir,"data2D.tsv")
-data2D = readtable(fname2D, delim='\t', coordnames=[:x,:y])
-fname3D = joinpath(datadir,"data3D.tsv")
-data3D = readtable(fname3D, delim='\t')
-
-# missing data and NaN
-fname = joinpath(datadir,"missing.tsv")
-missdata = readtable(fname, delim='\t', coordnames=[:x,:y])
-
-# more realistic data
-fname = joinpath(datadir,"samples2D.tsv")
-samples2D = readtable(fname, delim='\t', coordnames=[:x,:y])
+# load data sets
+data1D = readtable(joinpath(datadir,"data1D.tsv"), delim='\t', coordnames=[:x])
+data2D = readtable(joinpath(datadir,"data2D.tsv"), delim='\t', coordnames=[:x,:y])
 
 # list of tests
 testfiles = [
-  "distances.jl",
-  "distributions.jl",
-  "spatialdata.jl",
-  "domains.jl",
-  "paths.jl",
-  "neighborhoods.jl",
-  "mappers.jl",
-  "problems.jl",
   "solvers.jl",
-  "comparisons.jl",
-  "plotrecipes.jl"
+  "comparisons.jl"
 ]
 
 @testset "GeoStats.jl" begin
