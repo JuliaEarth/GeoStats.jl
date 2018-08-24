96775da58425b0a86f661306f5e2ff00c142e899
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-08-24 16:49:18 -0700

    Only test main project module
    Former-commit-id: f2c0fb714403b4265b3817403812a7084f9cf915

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,20 +12,6 @@ istravis = "TRAVIS" ∈ keys(ENV)
 ismaintainer = "USER" ∈ keys(ENV) && ENV["USER"] ∈ maintainers
 datadir = joinpath(@__DIR__,"data")
 
-# test project modules
-# if !istravis
-if false
-  println()
-  @info "----- TESTING PROJECT MODULES -----"
-  println()
-  for pkg in ["GeoStatsBase","GeoStatsDevTools"]
-    Pkg.test(pkg)
-    println()
-  end
-  @info "----- TESTING MAIN MODULE -----"
-  println()
-end
-
 # load data sets
 data1D = readtable(joinpath(datadir,"data1D.tsv"), delim='\t', coordnames=[:x])
 data2D = readtable(joinpath(datadir,"data2D.tsv"), delim='\t', coordnames=[:x,:y])
