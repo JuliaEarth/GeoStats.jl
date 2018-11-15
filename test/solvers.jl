2c9211bfdb40eccdc9912d11fc73a2e56c03f7e0
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-11-15 18:46:38 -0200

    Add more tests for sgsim
    Former-commit-id: 801b7f96b8b0548c1d107d088ec2c20e991c0287

--- test/solvers.jl
+++ test/solvers.jl
@@ -35,33 +35,37 @@
 
   @testset "SeqGaussSim" begin
     @testset "Conditional" begin
-      problem1D = SimulationProblem(data1D, grid1D, :value, 1)
-      problem2D = SimulationProblem(data2D, grid2D, :value, 1)
-      solver = SeqGaussSim(:value => (variogram=GaussianVariogram(range=35.),))
+      nreals = 3
 
-      # Random.seed!(2017)
-      # solution1D = solve(problem1D, solver)
-      # solution2D = solve(problem2D, solver)
+      problem1D = SimulationProblem(data1D, grid1D, :value, nreals)
+      problem2D = SimulationProblem(data2D, grid2D, :value, nreals)
+      solver = SeqGaussSim(:value => (variogram=SphericalVariogram(range=35.),))
 
-      # # basic checks
-      # result = digest(solution2D)
-      # @test Set(keys(result)) == Set([:value])
-      # @test result[:value][1][26,26] == 1.
-      # @test result[:value][1][51,76] == 0.
-      # @test result[:value][1][76,51] == 1.
+      Random.seed!(2017)
+      solution1D = solve(problem1D, solver)
+      solution2D = solve(problem2D, solver)
+
+      # basic checks
+      result = digest(solution2D)
+      @test Set(keys(result)) == Set([:value])
+      for i=1:nreals
+        @test result[:value][i][26,26] == 1.
+        @test result[:value][i][51,76] == 0.
+        @test result[:value][i][76,51] == 1.
+      end
 
       if ismaintainer || istravis
         @testset "Plot recipe" begin
           function plot_solution(fname, solution)
-            plot(solution, size=(800,400))
+            plot(solution, size=(1000,300))
             png(fname)
           end
           plot_sol1D(fname) = plot_solution(fname, solution1D)
           plot_sol2D(fname) = plot_solution(fname, solution2D)
           refimg1D = joinpath(datadir,"SeqGaussSimSolution1D.png")
           refimg2D = joinpath(datadir,"SeqGaussSimSolution2D.png")
-          #@test test_images(VisualTest(plot_sol1D, refimg1D), popup=!istravis) |> success
-          #@test test_images(VisualTest(plot_sol2D, refimg2D), popup=!istravis) |> success
+          # @test test_images(VisualTest(plot_sol1D, refimg1D), popup=!istravis) |> success
+          # @test test_images(VisualTest(plot_sol2D, refimg2D), popup=!istravis) |> success
         end
       end
     end
