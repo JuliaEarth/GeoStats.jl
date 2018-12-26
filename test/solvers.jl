981eb2c2620a8404ab297cb256951e21ed6964e2
Júlio Hoffimann <julio.hoffimann@gmail.com>
2018-12-26 18:00:08 -0200

    Update test suite
    Former-commit-id: 3a89e992ce7ec0e8265a91d0fce7cb7fb518aa84

--- test/solvers.jl
+++ test/solvers.jl
@@ -5,7 +5,7 @@
   @testset "Kriging" begin
     problem1D = EstimationProblem(data1D, grid1D, :value)
     problem2D = EstimationProblem(data2D, grid2D, :value)
-    solver = Kriging(:value => (variogram=GaussianVariogram(range=35.),))
+    solver = Kriging(:value => (variogram=GaussianVariogram(range=35.,nugget=0.),))
 
     solution1D = solve(problem1D, solver)
     solution2D = solve(problem2D, solver)
@@ -34,12 +34,15 @@
   end
 
   @testset "SeqGaussSim" begin
-    @testset "Conditional" begin
       nreals = 3
-
+      function plot_solution(fname, solution)
+        plot(solution, size=(1000,300))
+        png(fname)
+      end
+    @testset "Conditional" begin
       problem1D = SimulationProblem(data1D, grid1D, :value, nreals)
       problem2D = SimulationProblem(data2D, grid2D, :value, nreals)
-      solver = SeqGaussSim(:value => (variogram=SphericalVariogram(range=35.),))
+      solver = SeqGaussSim(:value => (variogram=GaussianVariogram(range=35.),))
 
       Random.seed!(2017)
       solution1D = solve(problem1D, solver)
@@ -56,14 +59,10 @@
 
       if ismaintainer || istravis
         @testset "Plot recipe" begin
-          function plot_solution(fname, solution)
-            plot(solution, size=(1000,300))
-            png(fname)
-          end
           plot_sol1D(fname) = plot_solution(fname, solution1D)
           plot_sol2D(fname) = plot_solution(fname, solution2D)
-          refimg1D = joinpath(datadir,"SeqGaussSimSolution1D.png")
-          refimg2D = joinpath(datadir,"SeqGaussSimSolution2D.png")
+          refimg1D = joinpath(datadir,"SGSCond1D.png")
+          refimg2D = joinpath(datadir,"SGSCond2D.png")
           # @test test_images(VisualTest(plot_sol1D, refimg1D), popup=!istravis) |> success
           # @test test_images(VisualTest(plot_sol2D, refimg2D), popup=!istravis) |> success
         end
@@ -71,12 +70,24 @@
     end
 
     @testset "Unconditional" begin
-      problem1D = SimulationProblem(grid1D, :value => Float64, 1)
-      problem2D = SimulationProblem(grid2D, :value => Float64, 1)
+      problem1D = SimulationProblem(grid1D, :value => Float64, nreals)
+      problem2D = SimulationProblem(grid2D, :value => Float64, nreals)
       solver = SeqGaussSim(:value => (variogram=GaussianVariogram(range=35.),))
 
-      #solution2D = solve(problem2D, SeqGaussSim())
-      # TODO: test solution correctness
+      Random.seed!(2017)
+      solution1D = solve(problem1D, solver)
+      solution2D = solve(problem2D, solver)
+
+      if ismaintainer || istravis
+        @testset "Plot recipe" begin
+          plot_sol1D(fname) = plot_solution(fname, solution1D)
+          plot_sol2D(fname) = plot_solution(fname, solution2D)
+          refimg1D = joinpath(datadir,"SGSUncond1D.png")
+          refimg2D = joinpath(datadir,"SGSUncond2D.png")
+          # @test test_images(VisualTest(plot_sol1D, refimg1D), popup=!istravis) |> success
+          # @test test_images(VisualTest(plot_sol2D, refimg2D), popup=!istravis) |> success
+        end
+      end
     end
   end
 
