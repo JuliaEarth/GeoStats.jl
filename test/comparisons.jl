b4db111d766de7d46b3a63041078183880f63f4e
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-01-13 16:44:00 -0200

    Update test suite

--- test/comparisons.jl
+++ test/comparisons.jl
@@ -1,10 +1,12 @@
 @testset "Comparisons" begin
-  grid2D = RegularGrid{Float64}(100,100)
-  problem2D = EstimationProblem(data2D, grid2D, :value)
-  solver₁ = Kriging(:value => (variogram=GaussianVariogram(range=35.),))
-  solver₂ = Kriging(:value => (variogram=SphericalVariogram(range=35.),))
-
   @testset "Visual comparison" begin
+    data2D = readgeotable(joinpath(datadir,"data2D.tsv"), delim='\t', coordnames=[:x,:y])
+    grid2D = RegularGrid{Float64}(100,100)
+    problem2D = EstimationProblem(data2D, grid2D, :value)
+
+    solver₁ = Kriging(:value => (variogram=GaussianVariogram(range=35.),))
+    solver₂ = Kriging(:value => (variogram=SphericalVariogram(range=35.),))
+
     if ismaintainer || istravis
       function plot_comparison(fname)
         compare([solver₁, solver₂], problem2D, VisualComparison())
@@ -16,6 +18,13 @@
   end
 
   @testset "Cross-validation" begin
+    data2D = readgeotable(joinpath(datadir,"data2D.tsv"), delim='\t', coordnames=[:x,:y])
+    grid2D = RegularGrid{Float64}(100,100)
+    problem2D = EstimationProblem(data2D, grid2D, :value)
+
+    solver₁ = Kriging(:value => (variogram=GaussianVariogram(range=40.),))
+    solver₂ = Kriging(:value => (variogram=SphericalVariogram(range=20.),))
+
     result = compare([solver₁, solver₂], problem2D, CrossValidation(3))
     errors4var = result.errors4var
 
@@ -24,6 +33,22 @@
     @test length(errors4var[:value][2]) == 3
 
     # number of folds must be smaller than number of points, throw an error
-    @test_throws AssertionError compare([solver₁, solver₂], problem2D, CrossValidation())
+    @test_throws AssertionError compare([solver₁, solver₂], problem2D, CrossValidation(10))
+
+    Random.seed!(2019)
+
+    data2D = readgeotable(joinpath(datadir,"500points.csv"), coordnames=[:x,:y])
+    grid2D = boundgrid(data2D, (100,100))
+    problem2D = EstimationProblem(data2D, grid2D, :value)
+
+    if ismaintainer || istravis
+      function plot_comparison(fname)
+        result = compare([solver₁, solver₂], problem2D, CrossValidation(10))
+        plot(result, bins=50)
+        png(fname)
+      end
+      refimg = joinpath(datadir,"CrossValidationn.png")
+      @test test_images(VisualTest(plot_comparison, refimg), popup=!istravis, tol=0.1) |> success
+    end
   end
 end
