da7050600b50ce64cadaffa6e572aaa8f4facdf8
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-04-18 20:45:12 -0300

    Update test data

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,9 @@ using Plots; gr(size=(600,400))
 using VisualRegressionTests
 using Test, Pkg, Random
 
+# workaround GR warnings
+ENV["GKSwstype"] = "100"
+
 # environment settings
 islinux = Sys.islinux()
 istravis = "TRAVIS" ∈ keys(ENV)
