1d91b41df0bf7ee12faeeae8e16a1850723a4c1e
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-05-11 19:27:46 -0300

    Update test data

--- test/comparisons.jl
+++ test/comparisons.jl
@@ -10,8 +10,9 @@
     if visualtests
       @plottest begin
         gr(size=(600,400))
+        Random.seed!(123)
         compare([solver₁, solver₂], problem2D, VisualComparison())
-      end joinpath(datadir,"VisualComparison.png") !istravis 0.1
+      end joinpath(datadir,"VisualComparison.png") !istravis
     end
   end
 
@@ -20,7 +21,7 @@
     grid2D = RegularGrid{Float64}(100,100)
     problem2D = EstimationProblem(data2D, grid2D, :value)
 
-    solver₁ = Kriging(:value => (variogram=GaussianVariogram(range=40.),))
+    solver₁ = Kriging(:value => (variogram=GaussianVariogram(range=20.),))
     solver₂ = Kriging(:value => (variogram=SphericalVariogram(range=20.),))
 
     result = compare([solver₁, solver₂], problem2D, CrossValidation(3))
@@ -35,7 +36,7 @@
 
     Random.seed!(2019)
 
-    data2D = readgeotable(joinpath(datadir,"500points.csv"), coordnames=[:x,:y])
+    data2D = readgeotable(joinpath(datadir,"500Gaussian.csv"), coordnames=[:x,:y])
     grid2D = boundgrid(data2D, (100,100))
     problem2D = EstimationProblem(data2D, grid2D, :value)
 
@@ -44,7 +45,7 @@
         gr(size=(600,400))
         result = compare([solver₁, solver₂], problem2D, CrossValidation(10))
         plot(result, bins=50)
-      end joinpath(datadir,"CrossValidation.png") !istravis 0.1
+      end joinpath(datadir,"CrossValidation.png") !istravis
     end
   end
 end
