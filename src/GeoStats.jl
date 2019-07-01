022bc51355280962f5b87cc5382b0edfc8c8d6a7
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-06-30 21:01:25 -0300

    Remove direct dependency on Distances.jl and Distributions.jl
    Former-commit-id: e46f87e76309c3e91f77ba13e5b520f182f7d2e2

--- src/GeoStats.jl
+++ src/GeoStats.jl
@@ -6,10 +6,6 @@ module GeoStats
 
 using Reexport
 
-# reexport basic packages
-@reexport using Distances
-@reexport using Distributions
-
 # reexport project modules
 @reexport using GeoStatsBase
 @reexport using Variography
