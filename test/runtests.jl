c3db7af8c0000dd8acfbfec5a2cc0b356e5075d0
Júlio Hoffimann <julio.hoffimann@gmail.com>
2019-09-15 19:13:54 -0300

    Update runtests.jl
    Former-commit-id: 3f741903cd413716bbba4aa1eab8b9eae3e36f49

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,6 @@
 using GeoStats
 using Test, Pkg, Random
 
-# workaround GR warnings
-ENV["GKSwstype"] = "100"
-
-# environment settings
-islinux = Sys.islinux()
-istravis = "TRAVIS" ∈ keys(ENV)
-datadir = joinpath(@__DIR__,"data")
-visualtests = !istravis || (istravis && islinux)
-if !istravis
-  Pkg.add("Gtk")
-  using Gtk
-end
-
 # list of tests
 testfiles = [
 ]
